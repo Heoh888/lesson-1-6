5741296986799c4dc172cf4fc6bd1dbe8c70523b
alexfit <heoh888@ya.ru>
2021-01-04 13:53:50 +0300

    added a change

--- lesson-1-6/main.swift
+++ lesson-1-6/main.swift
@@ -6,49 +6,39 @@
 
 import Foundation
 
-protocol Distribution {
-    func timeLimit() -> Int
-}
 
-class Parking: Distribution {
-    var carNumber: String = generatingCarNumber()
+// Парковка для автомобилей
+class Parking: CustomStringConvertible {
+    var carNumber: String = generatingCarNumber() // генерирует случайный номер автомобиля
     var paidForParking: Int
     var parkingSpace = Int.random(in: 0..<100)
     
     init(paidForParking: Int) {
         self.paidForParking = paidForParking
     }
-    
+    // определяет на сколько оплачена парковка
     func timeLimit() -> Int {
         return paidForParking / 100
     }
+    var description: String {
+        return "Автомобиль \(carNumber) занял парковку №\(parkingSpace) оплачено \(timeLimit()) часа"
+    }
 }
-extension Parking: CustomStringConvertible {
-     var description: String {
-         return "Автомобиль \(carNumber) занял парковку №\(parkingSpace) оплачено \(timeLimit()) часа"
-     }
- }
 
-class Arena: Distribution {
+// Арена для персонажей
+class Arena: CustomStringConvertible {
     var nameCharacter: String
     var arenaNumber = Int.random(in: 1..<10)
     
     init(nameCharacter: String) {
         self.nameCharacter = nameCharacter
     }
-    
-    func timeLimit() -> Int {
-        let status: Int = 0
-        return status
+    var description: String {
+        return "Персонаж \(nameCharacter) занял арену №\(arenaNumber)"
     }
 }
 
-extension Arena: CustomStringConvertible {
-     var description: String {
-         return "Персонаж \(nameCharacter) занял арену №\(arenaNumber)"
-     }
- }
-
+// коллекция «очередь» для классов Arena и Parking
 struct Queue<T> {
     private var elemments: [T] = []
     
@@ -60,31 +50,24 @@ struct Queue<T> {
         return elemments.remove(at: 0)
     }
 }
-
+// расширение для коллекции (Queue: CustomStringConvertible)
 extension Queue: CustomStringConvertible {
      var description: String {
         return elemments.isEmpty ? "Очередь пуста" : "Очередь: \(elemments)"
      }
  }
 
-extension Queue {
-    
-    func filter() {
-        
-    }
-    
- }
 
 var parkingCar = Queue<Parking>()
 var сharacterArena = Queue<Arena>()
 
-сharacterArena.push(Arena(nameCharacter: "Konnon"))
-сharacterArena.push(Arena(nameCharacter: "Ragnar"))
-сharacterArena.push(Arena(nameCharacter: "Anny"))
-print(сharacterArena)
-
-let elementCharacter1 = сharacterArena.pop()
-print(сharacterArena)
+//сharacterArena.push(Arena(nameCharacter: "Konnon"))
+//сharacterArena.push(Arena(nameCharacter: "Ragnar"))
+//сharacterArena.push(Arena(nameCharacter: "Anny"))
+//print(сharacterArena)
+//
+//let elementCharacter1 = сharacterArena.pop()
+//print(сharacterArena)
 
 parkingCar.push(Parking(paidForParking: 100))
 parkingCar.push(Parking(paidForParking: 200))
@@ -95,6 +78,7 @@ let elementCar1 = parkingCar.pop()
 let elementCar2 = parkingCar.pop()
 let elementCar3 = parkingCar.pop()
 print(parkingCar)
+
 
 
 //print(randonNumber())
