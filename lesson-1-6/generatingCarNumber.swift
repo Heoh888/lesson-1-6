7e2cb06d2e140732453265e4a2bb966de5631251
alexfit <heoh888@ya.ru>
2021-01-04 13:31:52 +0300

    Revert "Initial Commit"
    This reverts commit c62e672890d1fa0fa2897cfcafd5118acebe7af1.

---
+++ lesson-1-6/generatingCarNumber.swift
@@ -0,0 +1,19 @@
+//
+//  generatingCarNumber.swift
+//  lesson-1-6
+//
+//  Created by MacBook on 04.01.2021.
+//
+
+import Foundation
+
+public func generatingCarNumber() -> String {
+    let alphabeticalArray = [ "а", "в", "е", "к", "м", "н", "о", "р", "с", "т", "у", "х"]
+    let arrayRegions = ["77", "99", "97", "177", "199", "197", "777", "799", "797"]
+   
+    let generation = (seriesSymbol1: alphabeticalArray.randomElement()!, registrationNumber: Int.random(in: 100..<999), seriesSymbol2: alphabeticalArray.randomElement()!, seriesSymbol3: alphabeticalArray.randomElement()!, region: arrayRegions.randomElement()!)
+    
+    let number: String = generation.seriesSymbol1  + String(generation.registrationNumber) + generation.seriesSymbol2 + generation.seriesSymbol3 + generation.region
+    
+    return number
+}
--- lesson-1-6/main.swift
+++ lesson-1-6/main.swift
@@ -1,11 +1,101 @@
+//1. Реализовать свой тип коллекции «очередь» (queue) c использованием дженериков.
 //
-//  main.swift
-//  lesson-1-6
+//2. Добавить ему несколько методов высшего порядка, полезных для этой коллекции (пример: filter для массивов)
 //
-//  Created by MacBook on 03.01.2021.
-//
+//3. * Добавить свой subscript, который будет возвращать nil в случае обращения к несуществующему индексу.
 
 import Foundation
 
-print("Hello, World!")
+protocol Distribution {
+    func timeLimit() -> Int
+}
+
+class Parking: Distribution {
+    var carNumber: String = generatingCarNumber()
+    var paidForParking: Int
+    var parkingSpace = Int.random(in: 0..<100)
+    
+    init(paidForParking: Int) {
+        self.paidForParking = paidForParking
+    }
+    
+    func timeLimit() -> Int {
+        return paidForParking / 100
+    }
+}
+extension Parking: CustomStringConvertible {
+     var description: String {
+         return "Автомобиль \(carNumber) занял парковку №\(parkingSpace) оплачено \(timeLimit()) часа"
+     }
+ }
+
+class Arena: Distribution {
+    var nameCharacter: String
+    var arenaNumber = Int.random(in: 1..<10)
+    
+    init(nameCharacter: String) {
+        self.nameCharacter = nameCharacter
+    }
+    
+    func timeLimit() -> Int {
+        let status: Int = 0
+        return status
+    }
+}
+
+extension Arena: CustomStringConvertible {
+     var description: String {
+         return "Персонаж \(nameCharacter) занял арену №\(arenaNumber)"
+     }
+ }
+
+struct Queue<T> {
+    private var elemments: [T] = []
+    
+    mutating func push(_ elemment: T) {
+        elemments.append(elemment)
+    }
+    
+    mutating func pop() -> T {
+        return elemments.remove(at: 0)
+    }
+}
+
+extension Queue: CustomStringConvertible {
+     var description: String {
+        return elemments.isEmpty ? "Очередь пуста" : "Очередь: \(elemments)"
+     }
+ }
+
+extension Queue {
+    
+    func filter() {
+        
+    }
+    
+ }
+
+var parkingCar = Queue<Parking>()
+var сharacterArena = Queue<Arena>()
+
+сharacterArena.push(Arena(nameCharacter: "Konnon"))
+сharacterArena.push(Arena(nameCharacter: "Ragnar"))
+сharacterArena.push(Arena(nameCharacter: "Anny"))
+print(сharacterArena)
+
+let elementCharacter1 = сharacterArena.pop()
+print(сharacterArena)
+
+parkingCar.push(Parking(paidForParking: 100))
+parkingCar.push(Parking(paidForParking: 200))
+parkingCar.push(Parking(paidForParking: 300))
+print(parkingCar)
+
+let elementCar1 = parkingCar.pop()
+let elementCar2 = parkingCar.pop()
+let elementCar3 = parkingCar.pop()
+print(parkingCar)
+
+
+//print(randonNumber())
 
